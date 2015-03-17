17aa63b29aa1f79080e71a1a4be9e840feb1cc00
Jed Chou <jedchou1@illinois.edu>
2015-03-16 22:44:57 -0500

    adds script with FIFO commands to run the PAUP version of SVDquartets

--- get_quartet_svd_scores.sh
+++ get_quartet_svd_scores.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./paup4a142_osx_leopard
+FNAME=get_quartet_svd_scores.fifo
+mkfifo $FNAME 
+./paup4a142_osx_leopard < $FNAME &
+echo "tonexus format=PHYLIP fromfile=data.phy tofile=data.nex;"  > $FNAME
+echo "Execute data.nex;" > $FNAME
+echo "SVDQuartets showScores=yes nquartets=50;" > $FNAME
